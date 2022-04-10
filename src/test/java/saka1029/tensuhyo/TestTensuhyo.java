b00bace97cb862e111e040df33f26f9f3ce9a22e
saka1029 <saka1029@gmail.com>
2022-04-10 20:48:16 +0900

    settings.jsonの改行コードをLFに

---
+++ src/test/java/saka1029/tensuhyo/TestTensuhyo.java
@@ -0,0 +1,11 @@
+package saka1029.tensuhyo;
+
+import org.junit.Test;
+
+public class TestTensuhyo {
+
+    @Test
+    public void testTensuhyo() {
+        System.out.println("これはtestTensuhyoです");
+    }
+}
