ec1bfa288257c684cd0f691c7220598540046e47
saka1029 <saka1029@gmail.com>
2022-04-16 07:28:33 +0900

    Main変更

--- src/main/java/saka1029/tensuhyo/main/Main.java
+++ src/main/java/saka1029/tensuhyo/main/Main.java
@@ -7,10 +7,40 @@ import java.io.File;
  */
 public class Main {
 
-    static String USAGE = "usage java saka1029.tensuhyo.main.Main [-d TOP_DIRECTORY] PDF_CONFIG OPERATION...";
+    static String USAGE = "usage java saka1029.tensuhyo.main.Main [-d TOP_DIRECTORY] PDF_CONFIG OPERATION...%n"
+        + "  TOP_DIRECTORY: 先頭ディレクトリ%n"
+        + "    TOP_DIRECTORY%n"
+        + "      +---data%n"
+        + "          +---in%n"
+        + "          |   +---04 (令和4年)%n"
+        + "          |       +---k (施設基準)%n"
+        + "          |       |   +---pdf%n"
+        + "          |       |   +---txt%n"
+        + "          |       +---i (医科)%n"
+        + "          |       |   +---pdf%n"
+        + "          |       |   +---txt%n"
+        + "          |       +---s (歯科)%n"
+        + "          |       |   +---pdf%n"
+        + "          |       |   +---txt%n"
+        + "          |       +---s (調剤)%n"
+        + "          |           +---pdf%n"
+        + "          |           +---txt%n"
+        + "          +---web%n"
+        + "              +---04 (令和4年)%n"
+        + "  PDF_CONFIG: PDFの場所を格納する定義ファイル%n"
+        + "  OPERATION:%n"
+        + "    k0 施設基準PDF変換%n"
+        + "    k1 施設基準HTML生成%n"
+        + "    i0 医科PDF変換%n"
+        + "    i1 医科HTML生成%n"
+        + "    s0 歯科PDF変換%n"
+        + "    s1 歯科HTML生成%n"
+        + "    t0 調剤PDF変換%n"
+        + "    i1 調剤HTML生成%n"
+        ;
 
-    static void uasge() {
-        System.err.println(USAGE);
+    static void usage() {
+        System.err.printf(USAGE);
         System.exit(1);
     }
 
@@ -24,6 +54,7 @@ public class Main {
             } else
                 break;
         }
-        
+        if (max - i < 2)
+            usage();
     }
 }
