737cbcefcc10ff700720ee1a00c1fa80efe8fd36
saka1029 <saka1029@gmail.com>
2022-04-15 21:10:32 +0900

    Update Main.java

--- src/main/java/saka1029/tensuhyo/main/Main.java
+++ src/main/java/saka1029/tensuhyo/main/Main.java
@@ -1,7 +1,29 @@
 package saka1029.tensuhyo.main;
 
+import java.io.File;
+
+/**
+ * usage: java saka1029.tensuhyo.main.Main [-d TOP_DIRECTORY] PDF_CONFIG OPERATION...
+ */
 public class Main {
+
+    static String USAGE = "usage java saka1029.tensuhyo.main.Main [-d TOP_DIRECTORY] PDF_CONFIG OPERATION...";
+
+    static void uasge() {
+        System.err.println(USAGE);
+        System.exit(1);
+    }
+
     public static void main(String[] args) {
+        File top = new File(".");
+        int max = args.length, i = 0;
+        for ( ; i < max; ++i) {
+            String arg = args[i];
+            if (arg.startsWith("-")) {
+
+            } else
+                break;
+        }
         
     }
 }
