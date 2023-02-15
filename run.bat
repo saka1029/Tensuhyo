9afb5e1df398c0409b0680456c8a6f659607a32d
saka1029 <saka1029@gmail.com>
2023-02-16 08:34:35 +0900

    pom.xml更新

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 setlocal
 
 java -Djava.util.logging.config.file=logging.properties ^
-    -cp lib/*;target/tensuhyo-1.0.jar ^
+    -cp target/tensuhyo-1.0-jar-with-dependencies.jar ^
     saka1029.tensuhyo.main.Main ^
     %*
 
