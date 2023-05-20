a8cefa849afeed2de66f2dcb4de25ee240d8d975
saka1029 <saka1029@gmail.com>
2023-05-20 17:29:06 +0900

    Git上にないPNGファイルをサーバから復元した。(継続)

--- src/test/java/saka1029/tensuhyo/ftp/TestFtp.java
+++ src/test/java/saka1029/tensuhyo/ftp/TestFtp.java
@@ -31,7 +31,7 @@ public class TestFtp {
         }
     }
 
-    @Test
+//    @Test
     public void testRecoverPngFiles() throws IOException {
         String[] ftpConfig = System.getenv("FTP_CONFIG").split("\\s+");
         OUT.println(Arrays.toString(ftpConfig));
@@ -40,22 +40,22 @@ public class TestFtp {
         client.login(ftpConfig[1], ftpConfig[2]);
         try (Closeable c = () -> client.disconnect()) {
             client.setFileType(FTPClient.BINARY_FILE_TYPE);
-            getPngFiles(client, "30/k/image");
+//            getPngFiles(client, "30/k/image");
             getPngFiles(client, "30/i/image");
-            getPngFiles(client, "30/s/image");
-            getPngFiles(client, "30/t/image");
-            getPngFiles(client, "01/k/image");
-            getPngFiles(client, "01/i/image");
-            getPngFiles(client, "01/s/image");
-            getPngFiles(client, "01/t/image");
-            getPngFiles(client, "02/k/image");
-            getPngFiles(client, "02/i/image");
-            getPngFiles(client, "02/s/image");
-            getPngFiles(client, "02/t/image");
-            getPngFiles(client, "04/k/image");
-            getPngFiles(client, "04/i/image");
+//            getPngFiles(client, "30/s/image");
+//            getPngFiles(client, "30/t/image");
+//            getPngFiles(client, "01/k/image");
+//            getPngFiles(client, "01/i/image");
+//            getPngFiles(client, "01/s/image");
+//            getPngFiles(client, "01/t/image");
+//            getPngFiles(client, "02/k/image");
+//            getPngFiles(client, "02/i/image");
+//            getPngFiles(client, "02/s/image");
+//            getPngFiles(client, "02/t/image");
+//            getPngFiles(client, "04/k/image");
+//            getPngFiles(client, "04/i/image");
             getPngFiles(client, "04/s/image");
-            getPngFiles(client, "04/t/image");
+//            getPngFiles(client, "04/t/image");
         }
     }
 
